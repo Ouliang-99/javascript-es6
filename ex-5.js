910175e46c383e727437bc18ced0c6e680ce6ebd
podusit <d.rimdusit@gmail.com>
2024-10-15 14:50:51 +0700

    hackhour:es6 ex1-7

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,7 @@
-function sum() {
+function sum(...param) {
   // Start coding here !
+  return param.reduce((acc,cur) => acc + cur,0);
+
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,7 @@
 let height = undefined;
 let result;
-
+function myHeight(height){
+    result = height ?? "Height is not defined";
+}
+myHeight(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
