be0907ce6ec010aa3550cc4dc8d8c34629835d8f
Thanate Dokmai <nook.microstock@gmail.com>
2024-10-15 14:55:03 +0700

    ES6-Nuk

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,7 @@
 let height = undefined;
 let result;
-
+function nullCheck(height) {
+  return (result = height ?? "Height is not defined");
+}
+nullCheck(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
